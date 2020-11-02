a9b3378ffb244151deda984b50215f58f61d8f0b
exuanbo <exuanbo@protonmail.com>
2020-11-02 20:28:59 +0000

    test: add `extensions: ['ts']` to `cmd.js`

--- test/cmd.js
+++ test/cmd.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 
 const { cli } = require('..')
-cli({ useGitIgnore: true, configFile: './.eslintrc.js' })
+cli({ extensions: ['ts'], configFile: './.eslintrc.js', useGitIgnore: true })
