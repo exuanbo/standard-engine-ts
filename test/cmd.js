59e8f0683fad7b0d34770db5a313cf31b1143037
exuanbo <exuanbo@protonmail.com>
2020-11-03 22:05:39 +0000

    test: require eslint

--- test/cmd.js
+++ test/cmd.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
 const path = require('path')
+const eslint = require('eslint')
 const { cli } = require('..')
 
 cli({
+  eslint,
   extensions: ['.ts'],
   configFile: path.join(__dirname, '..', '.eslintrc.js'),
   useGitIgnore: true
