2ab09f3f2151e813f3c656017264cb505a0e43c9
exuanbo <exuanbo@protonmail.com>
2020-11-23 16:11:35 +0000

    chore: add `@typescript-eslint/recommended`

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
       files: ['**/*.ts'],
       extends: [
         'standard-with-typescript',
+        'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
         'prettier/@typescript-eslint'
       ],
