d2ce7fb6948a1bd1b8c38f77b9e2dadb503a3a0d
exuanbo <exuanbo@protonmail.com>
2021-09-15 15:11:24 +0100

    chore: use `ts-standardx` eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,3 @@
 module.exports = {
-  extends: ['standard', 'plugin:prettier/recommended'],
-  overrides: [
-    {
-      files: ['**/*.ts'],
-      extends: ['plugin:@typescript-eslint/recommended', 'standard-with-typescript'],
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        project: './tsconfig.json'
-      },
-      rules: {
-        '@typescript-eslint/indent': 'off',
-        '@typescript-eslint/space-before-function-paren': 'off'
-      }
-    }
-  ]
+  extends: ['./node_modules/ts-standardx/.eslintrc.js']
 }
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,3 @@
 module.exports = {
-  printWidth: 100,
-  semi: false,
-  singleQuote: true,
-  trailingComma: 'none',
-  jsxBracketSameLine: true,
-  arrowParens: 'avoid'
+  printWidth: 100
 }
